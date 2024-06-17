c96301c0b4587960e99c3c496e6e7f3e6e4c275c
David Yoon <davidyoon@hanwha.com>
2024-06-17 13:41:06 +0900

    Initial Commit

---
+++ AdmobSwiftUISample/AdmobSwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdmobSwiftUISampleApp.swift
+//  AdmobSwiftUISample
+//
+//  Created by Davidyoon on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct AdmobSwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
